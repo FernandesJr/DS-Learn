fd3966a5f6487d013ee016cb6d344b3016fc5840
Fernandes Júnior <fernaandes.jr@gmail.com>
2022-03-02 16:18:01 -0300

    Created Course and Offer with ORM

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,7 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML AND CSS', 'https://ayltoninacio.com.br/img/p/32w750.jpg', 'https://images.squarespace-cdn.com/content/v1/5551ffc1e4b006bc9d6ed809/1590950706330-UKY1Z9XQX6IEUTHOA6EK/html-css.jpg');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2022-02-03T00:00:00Z', TIMESTAMP WITH TIME ZONE '2023-02-03T00:00:00Z', 1);
