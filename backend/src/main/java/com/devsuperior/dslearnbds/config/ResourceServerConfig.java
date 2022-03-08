9fdd88b7225b0a476a43bb29b22ead06e33b8e60
Fernandes Júnior <fernaandes.jr@gmail.com>
2022-03-08 20:42:11 -0300

    Implement Security, Validation and Token

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/config/ResourceServerConfig.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dslearnbds.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private Environment environment; //Ambiente de execução, nesta class focado para config o db h2
+
+    public static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"}; //login
+
+    public static final String ROLE_ADMIN = "ADMIN";
+    public static final String ROLE_STUDENT = "OPERATOR";
+    public static final String ROLE_INSTRUCTOR = "INSTRUCTOR";
+
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .anyRequest().authenticated(); //Informando que qualquer outra rota não especificada será necessário se autenticar
+
+        //Configuração especial para rodar o H2 com a segurança dos recursos
+        if(Arrays.asList(environment.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
+    }
+}
---
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.devsuperior.dslearnbds.resources.exceptions;
+
+import com.devsuperior.dslearnbds.services.exceptions.DataBaseException;
+import com.devsuperior.dslearnbds.services.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice //Escuta as exceptions lançadas nos Controllers
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class) //Capturando exception específica
+    public ResponseEntity<StandarError> notFoundExceptionResponseEntity(ResourceNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandarError error = new StandarError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Resource not found");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(DataBaseException.class) //Capturando exception específica
+    public ResponseEntity<StandarError> database(DataBaseException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandarError error = new StandarError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Resource not found");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) //Capturando exception específica create or update the user
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY; //Significa que a entidade não pôde ser processada 422
+        ValidationError error = new ValidationError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Validation Exception");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+        for(FieldError f : e.getBindingResult().getFieldErrors()){
+            error.addErros(f.getField(), f.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status).body(error);
+    }
+}
